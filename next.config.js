eebc19c86e5f064cc96187fe59d751383999d706
Sviatoslav O <s.terletskyi@ocado.com>
2023-12-28 02:26:07 +0200

    testing cors fixes

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   async headers() {
     return [
       {
-        source: "/api/:path*",
+        source: "*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
