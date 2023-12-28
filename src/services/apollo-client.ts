1c97667706968d3414da7feee4e64a0dcf6a9ed1
Sviatoslav O <s.terletskyi@ocado.com>
2023-12-28 12:02:46 +0200

    undo

--- src/services/apollo-client.ts
+++ src/services/apollo-client.ts
@@ -14,9 +14,6 @@ export function makeClient() {
   });
 
   return new NextSSRApolloClient({
-    headers: {
-      'Origin': '*'
-    },
     cache: new NextSSRInMemoryCache(),
     link:
       typeof window === "undefined"
