a9d635c59b3970bd244d16c7bf1b5e23552a7c07
Sviatoslav O <s.terletskyi@ocado.com>
2023-12-28 21:31:54 +0200

    testing cors fix solution

--- next.config.js
+++ next.config.js
@@ -1,62 +1,16 @@
-// const nextConfig = {
-//     async headers() {
-//       return [
-//         {
-//           source: "/",
-//           headers: [
-//             { key: "X-Hi", value: "true" },
-//             // { key: "Access-Control-Allow-Origin", value: "*" },
-//             // { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-//             // { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-//           ],
-//         },
-//       ];
-//     },
-//   };
-  
-//   module.exports = nextConfig;
-  
-// const nextConfig = {
-//   async headers() {
-//     return [
-//       {
-//         source: '/(.*)',
-//         headers: [
-//           { key: "X-Hi", value: "true" },
-//           // other headers if necessary
-//         ],
-//       },
-//     ];
-//   },
-// };
-
-// module.exports = nextConfig;
-
-// next.config.js
 module.exports = {
-  async rewrites() {
+  async headers() {
     return [
       {
-        source: '/api/:path*',
-        destination: 'https://www.toiletmap.org.uk/api/:path*',
-      },
-    ];
-  },
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  }
 };
-
-// module.exports = {
-//   async headers() {
-//     return [
-//       {
-//         // matching all API routes
-//         source: "/api/:path*",
-//         headers: [
-//           { key: "Access-Control-Allow-Credentials", value: "true" },
-//           { key: "Access-Control-Allow-Origin", value: "*" },
-//           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-//           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-//         ]
-//       }
-//     ]
-//   }
-// };
