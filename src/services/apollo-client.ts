30ecd13f15598971431f85666d2f6f226646331e
Sviatoslav O <s.terletskyi@ocado.com>
2023-12-28 22:03:24 +0200

    testing cors fix solution

--- src/services/apollo-client.ts
+++ src/services/apollo-client.ts
@@ -11,7 +11,11 @@ import {
 export function makeClient() {
   const httpLink = new HttpLink({
     uri: "https://www.toiletmap.org.uk/api",
-    fetchOptions: { cache: "no-store" },
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+      'Access-Control-Allow-Headers': 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+    }
   });
 
   return new NextSSRApolloClient({
