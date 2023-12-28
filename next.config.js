33306cc43af3ea21ca69c067f991380415e7643c
Sviatoslav O <s.terletskyi@ocado.com>
2023-12-28 02:18:08 +0200

    undo webpack changes

--- next.config.js
+++ next.config.js
@@ -1,15 +1,4 @@
 module.exports = {
-    webpack: (config, { webpack }) => {
-      config.plugins.push(new webpack.IgnorePlugin({
-        resourceRegExp: /\/__tests__\//,
-      }));
-  
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-      });
-  
-      return config;
-    },
+    
   };
   
