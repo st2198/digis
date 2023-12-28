593d764aa01a492a0620e993bbaa32085cc79129
Sviatoslav O <s.terletskyi@ocado.com>
2023-12-28 21:04:26 +0200

    testing cors fix solution

--- next.config.js
+++ next.config.js
@@ -1,18 +1,47 @@
-const nextConfig = {
-    async headers() {
-      return [
-        {
-          source: "/api/:path*",
-          headers: [
-            { key: "Access-Control-Allow-Credentials", value: "true" },
-            { key: "Access-Control-Allow-Origin", value: "*" },
-            { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-            { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-          ],
-        },
-      ];
-    },
-  };
+// const nextConfig = {
+//     async headers() {
+//       return [
+//         {
+//           source: "/",
+//           headers: [
+//             { key: "X-Hi", value: "true" },
+//             // { key: "Access-Control-Allow-Origin", value: "*" },
+//             // { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+//             // { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+//           ],
+//         },
+//       ];
+//     },
+//   };
   
-  module.exports = nextConfig;
+//   module.exports = nextConfig;
   
+// const nextConfig = {
+//   async headers() {
+//     return [
+//       {
+//         source: '/(.*)',
+//         headers: [
+//           { key: "X-Hi", value: "true" },
+//           // other headers if necessary
+//         ],
+//       },
+//     ];
+//   },
+// };
+
+// module.exports = nextConfig;
+
+// next.config.js
+// module.exports = {
+//   async rewrites() {
+//     return [
+//       {
+//         source: '/api/:path*',
+//         destination: 'https://www.toiletmap.org.uk/api/:path*',
+//       },
+//     ];
+//   },
+// };
+
+module.exports = {};
